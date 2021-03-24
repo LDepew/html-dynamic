2f276ccc138884204762a9c38981d8145398b1cb
Lance Depew <Metal_Death_Music@yahoo.com>
2021-03-24 12:21:16 -0400

    create dynamic array

---
+++ js/dynamic.js
@@ -0,0 +1,14 @@
+let names = [
+    "Bryan", "Celina", "Lance", "Andrea", "Jonathan",
+    "Tyler", "Chas", "Dustin", "Pete", "Tom", 
+    "Will", "Daemon", "Jada", "Parker"
+];
+
+const loadDiv = () => {
+    let divCtrl = document.getElementById("ps");
+
+    for(let name of names) {
+        let p = `<p>${name}</p>`;
+        divCtrl.innerHTML += p;
+    }
+}
